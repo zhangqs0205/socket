5253d7d0462051cf63867dcfa9b440a918186191
zhangqs0205 <zhangqs0205@126.com>
2013-12-23 15:34:01 +0800

    commit

--- Common/JsonTool.cs
+++ Common/JsonTool.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using Contract;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Common
 {
-    class JsonTool
+    public static class JsonTool
     {
+        public static string SerializeMessage(Message messageObj)
+        {
+            return JsonConvert.SerializeObject(messageObj);
+        }
+        public static Message DeserializeMessage(string message)
+        {
+            return JsonConvert.DeserializeObject<Message>(message);
+        }
     }
 }
--- Common/PlaySound.cs
+++ Common/PlaySound.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Media;
 using System.Text;
 
 namespace Common
 {
-    class PlaySound
+    public class PlaySound
     {
+        public static void Play(byte[] buf)
+        {
+            MemoryStream ms = new MemoryStream(buf);
+            SoundPlayer sp = new SoundPlayer(ms);
+            sp.Play();
+        }
     }
 }
